26aea8b01e93e30ff7e0fe65cc6c4b8c030582d6
Joey <jlapegna@gmail.com>
2020-08-24 17:09:42 -0700

    Completed up to Lecture 123

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -1,7 +1,12 @@
 // Created in the Udemy Unreal Engine C++ Developer Course
 
+#include "DrawDebugHelpers.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
 
 #include "Grabber.h"
+
+#define OUT // OUT does nothing, but helps with readability for out parameters
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -18,10 +23,14 @@ UGrabber::UGrabber()
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	UE_LOG(LogTemp, Warning, TEXT("GRABBER REPORTING FOR DUTY!"));
 	
+	// Checking for the physics handler compontnet
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	
+	if (PhysicsHandle == nullptr)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Could not find UPhysicsHandleComponent in %s"), *GetOwner()->GetName());
+	}
 }
 
 
@@ -30,6 +39,28 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
-}
+	// Get the player's viewpoint
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation); // OUT does nothing, but helps with readability, we defined it above
 
+	// Draw a line from player, to show the reach
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	DrawDebugLine(GetWorld(), PlayerViewPointLocation, LineTraceEnd, FColor(0, 255, 0), false, 0.f, 0, 5);
+
+	// Raycast out to a certain distance (Reach)
+	FHitResult Hit;
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+	GetWorld()->LineTraceSingleByObjectType(OUT Hit, PlayerViewPointLocation, LineTraceEnd, FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody), TraceParams);
+
+	// Find what it's hitting
+	AActor* ActorHit = Hit.GetActor();
+	if (ActorHit != nullptr)
+	{
+		// Log to test
+		UE_LOG(LogTemp, Warning, TEXT("Raycast hit an actor named %s"), *ActorHit->GetName());
+	}
+
+	
+}
